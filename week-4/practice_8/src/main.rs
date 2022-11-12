2854be16668c4f5ff299bcae15d6671f5b6ebbd3
jackieanddiego <elijahduke89@gmail.com>
2022-11-12 10:02:21 -1200

    (My Project Commit)

--- week-4/practice_8/src/main.rs
+++ week-4/practice_8/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+     Let numl = 10 ;
+     Let num2 = 2 ; 
+     Let mut result:i32;
+
+result = numl + num2;
+println! ("Sum: {}}", res);
+
+result = num1 - num2; 
+println!("Difference: {} ", res);
+
+result = num1*num2 ; 
+println!("Product: {} ", res);
+
+result = numl/num2 ; 
+println!("Quotient: {} ", res);
+
+result = num1%num2 ;
+printin!("Remainder: {} ", res);
+
 }
---
+++ week-5/practice_7/target/debug/practice_7.d
@@ -0,0 +1,1 @@
+C:\Users\Duke\Documents\d.elijahCSC101\week-5\practice_7\target\debug\practice_7.exe: C:\Users\Duke\Documents\d.elijahCSC101\week-5\practice_7\src\main.rs
